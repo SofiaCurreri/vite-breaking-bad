54e934dab462afe2ea0cd2f8cb75208cc0241ad1
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-01 17:17:01 +0100

    main structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
+import CardList from "./components/CardList.vue";
 
 export default {
-  components: {
-    AppHeader,
-  },
+  components: { AppHeader, CardList },
 };
 </script>
 
 <template>
   <AppHeader />
 
-  <main></main>
+  <main class="d-flex align-items-end">
+    <div class="container">
+      <CardList />
+    </div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+  background-color: rgb(212, 143, 56);
+}
+
+.container {
+  background-color: white;
+  height: 90%;
+}
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      cards: [],
+    };
+  },
+
+  create() {
+    axios
+      .get(" https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
+      .then((response) => {
+        console.log(response);
+      });
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
