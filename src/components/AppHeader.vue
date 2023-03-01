6e6a788a035a6e035caf04ec0a38b7ad224daaf9
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-01 16:54:39 +0100

    header completed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+  <header class="d-flex align-items-center p-3">
+    <img src="../../public/img/yugioh-logo-the-art.png" alt="yugioh logo" />
+    <h1 class="ms-3">Yu-Gi-Oh Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+img {
+  width: 5rem;
+}
+</style>
