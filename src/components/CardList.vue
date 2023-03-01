fedabceb03c0a80cc5905f2cafb411371681f6f7
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-01 19:18:20 +0100

    cards completed

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -8,16 +8,44 @@ export default {
     };
   },
 
-  create() {
+  created() {
     axios
       .get(" https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
       .then((response) => {
         console.log(response);
+        this.cards = response.data;
       });
   },
 };
 </script>
 
-<template></template>
+<template>
+  <div class="row row-cols-5 justify-content-center p-3">
+    <div class="col text-center card m-3" v-for="card in cards.data">
+      <img :src="card.card_images[0].image_url" alt="" />
+      <div class="p-2">
+        <h4>{{ card.name }}</h4>
+        <p>{{ card.archetype }}</p>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 5 - 2rem);
+  background-color: rgb(212, 143, 56);
+  & h4 {
+    color: white;
+  }
+
+  & img {
+    width: 100%;
+  }
+}
+
+.row > * {
+  padding-left: 0;
+  padding-right: 0;
+}
+</style>
